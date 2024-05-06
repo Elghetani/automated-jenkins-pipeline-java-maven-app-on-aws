9567e2d374988dcfceb01f7f72737da5bf83fe0b
Elghetani <61852267+Elghetani@users.noreply.github.com>
2024-05-06 11:46:51 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh ' docker build --privileged -t elghetani/jenkins:jma-2.0 .'
+        sh ' docker build -t elghetani/jenkins:jma-2.0 .'
         sh " echo $PASS | docker login -u $USER --password-stdin"
         sh ' docker push elghetani/jenkins:jma-2.0'
     }
