188299ef8edba7df7f99db6aa740b89d92868f7f
Elghetani <61852267+Elghetani@users.noreply.github.com>
2024-05-11 16:03:19 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -16,7 +16,7 @@ def testApp() {
                 sh "exit 1"
             }
             catch (err) {                                        
-                failure(message: "${STAGE_NAME} is unstable")
+                unstable(message: "${STAGE_NAME} is unstable")
             }
 }
 def deployApp() {
